5ceec83343aa3277cbbc82378a600f4e7bf6b56d
Enchan <enchant0130@gmail.com>
2021-04-06 22:13:37 +0900

    [Update] fetch movies and comments

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Enchan1207/Serializable", from: "1.0.0"),
-        .package(url: "https://github.com/Enchan1207/YoutubeKit", from: "2.3.0"),
+        .package(url: "https://github.com/Enchan1207/YoutubeKit", from: "2.4.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/YoutubeSpamEliminator/YoutubeSpamEliminator.swift
+++ Sources/YoutubeSpamEliminator/YoutubeSpamEliminator.swift
@@ -1,6 +1,6 @@
 //
 //  YoutubeSpamEliminator.swift
-//  
+//
 //
 //  Created by EnchantCode on 2021/04/05.
 //
@@ -10,6 +10,7 @@ import Foundation
 
 final class YoutubeSpamEliminator {
     
+    let sema = DispatchSemaphore(value: 0)
     let fileManager = FileManager.default
     let youtube = YoutubeKit(apiCredential: API_CREDENTIAL, accessCredential: ACCESS_CREDENTIAL)
     
@@ -22,10 +23,71 @@ final class YoutubeSpamEliminator {
         guard let configData = fileManager.contents(atPath: configPath),
               let targets = Targets.deserialize(object: String(data: configData, encoding: .utf8)!) else {return EXIT_FAILURE}
         
-        // 各ターゲットについて
         let channels = targets.channels
-
         
+        // 各チャンネルについて
+        channels.forEach { (channel) in
+            
+            // 投稿日時順に10本検索し
+            let targetID = channel.id
+            var movies: [SearchResource] = []
+            youtube.search(maxResults: 10, channelId: targetID, option: ["type": "video", "order": "date"]) { (result) in
+                movies = result.items
+                self.sema.signal()
+            } failure: { (error) in
+                print(error)
+                self.sema.signal()
+            }
+            sema.wait()
+            
+            // それぞれのコメントスレッドを上から50件取得
+            movies.forEach { (movie) in
+                guard let targetID = movie.getID() else {return}
+                print(movie.snippet.title)
+                
+                //
+                let commentThreadSema = DispatchSemaphore(value: 0)
+                var threads: [CommentThreadResource] = []
+                self.youtube.getCommentThread(videoId: targetID, maxResults: 100, order: .relevance, textFormat: .plainText) { (result) in
+                    threads = result.items
+                    commentThreadSema.signal()
+                } failure: { (error) in
+                    print(error)
+                    commentThreadSema.signal()
+                }
+                commentThreadSema.wait()
+                
+                // 整形して表示
+                threads.forEach { (thread) in
+                    let textOriginal = thread.snippet!.topLevelComment!.snippet!.textOriginal!
+                    let textContent: String = textOriginal.replacingOccurrences(of: "\n", with: "\\n")
+                    let likeCount = thread.snippet!.topLevelComment!.snippet!.likeCount!
+                    
+                    print("  \(textContent) (\(likeCount) likes)")
+                    
+                    // さらに返信も取得
+                    let commentReplySema = DispatchSemaphore(value: 0)
+                    var replies: [CommentResource] = []
+                    self.youtube.getComment(parentID: thread.id, maxResults: 100, textFormat: .plainText) { (result) in
+                        replies = result.items
+                        commentReplySema.signal()
+                    } failure: { (error) in
+                        print(error)
+                        commentReplySema.signal()
+                    }
+                    commentReplySema.wait()
+                    
+                    replies.forEach { (reply) in
+                        let textOriginal = reply.snippet!.textOriginal!
+                        let textContent: String = textOriginal.replacingOccurrences(of: "\n", with: "\\n")
+                        let likeCount = reply.snippet!.likeCount!
+                        
+                        print("    \(textContent) (\(likeCount) likes)")
+                    }
+                }
+            }
+            
+        }
         return EXIT_SUCCESS
     }
     
